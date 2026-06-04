eb976fd8a1368c6d564e47adcc0c1af446fb7d29
Sagnik Hore <sagnikhoredht@gmail.com>
2026-06-04 15:07:47 +0530

    new Changes

--- src/test/java/tests/SauceDemoLoginTest.java
+++ src/test/java/tests/SauceDemoLoginTest.java
@@ -5,21 +5,28 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SauceDemoLoginTest {
 
     @Test
-    public void loginTest() throws InterruptedException {
+    public void loginTest() {
 
-        WebDriver driver = new EdgeDriver();
-      
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    
+
         driver.get("https://www.saucedemo.com/");
 
         driver.findElement(By.id("user-name"))
@@ -39,7 +46,6 @@ public class SauceDemoLoginTest {
         );
 
         System.out.println("Login Successful!");
-        
 
         driver.quit();
     }
