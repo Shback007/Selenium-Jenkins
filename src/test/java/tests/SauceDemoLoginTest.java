b49c6a2646a5a44e450f7014b2d3f1ac129a7bbb
Sagnik Hore <sagnikhoredht@gmail.com>
2026-06-04 14:44:01 +0530

    Selenium-Jenkins

---
+++ src/test/java/tests/SauceDemoLoginTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SauceDemoLoginTest {
+
+    @Test
+    public void loginTest() throws InterruptedException {
+
+        WebDriver driver = new EdgeDriver();
+      
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name"))
+                .sendKeys("standard_user");
+
+        driver.findElement(By.id("password"))
+                .sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button"))
+                .click();
+
+        String currentURL = driver.getCurrentUrl();
+
+        Assert.assertTrue(
+                currentURL.contains("inventory"),
+                "Login Failed!"
+        );
+
+        System.out.println("Login Successful!");
+        
+
+        driver.quit();
+    }
+}
